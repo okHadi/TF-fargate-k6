cc608467e72df02a033e39892d7cbc8a4c30b140
Hadi Khan <khan.hadi2951@gmail.com>
2023-04-18 16:38:20 +0500

    all necessary roles and containers being created

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+
+
+resource "aws_iam_policy_attachment" "ecs_task_execution_policy_attachment" {
+  name       = "ecs_task_execution_policy_attachment"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  roles      = ["ecsTaskExecutionRole"]
+}
+
+resource "aws_iam_policy_attachment" "cloudwatch_logs_full_access_policy_attachment" {
+  name       = "cloudwatch_logs_full_access_policy_attachment"
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+  roles      = ["ecsTaskExecutionRole"]
+}
+
+
+
+resource "aws_ecs_task_definition" "k6TF" {
+  family                   = "k6TF"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "1024"
+  memory                   = "2048"
+  execution_role_arn       = "arn:aws:iam::049964873288:role/ecsTaskExecutionRole"
+  container_definitions = jsonencode([
+    {
+      name  = "k6TFContainer"
+      image = "049964873288.dkr.ecr.ap-northeast-1.amazonaws.com/k6:latest"
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-create-group  = "true",
+          awslogs-group         = "/ecs/k6TF",
+          awslogs-region        = "ap-northeast-1",
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+
+}
+
+
+resource "aws_ecs_cluster" "k6TFCluster" {
+  name = "k6TFCluster"
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+  }
+}
